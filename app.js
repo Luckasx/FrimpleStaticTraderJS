62d21cc3f9a7208146d95894d31e7682a211617a
luckasx <luckasx@gmail.com>
2021-11-09 23:50:25 -0300

    added plotly and bootstrap cdns

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const port = 3000
 
 const router = require('./routes.js');
 
+
+app.use(express.static('static'));
+
 app.use("/", router);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes.js
+++ routes.js
@@ -2,14 +2,35 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/hello', (req, res) => {
-    res.send('Hello External World!')
+
+// require syntax (if your code base does not support imports)
+const yahooFinance = require('yahoo-finance2').default; // NOTE the .default
+
+
+//const results = await yahooFInance.search('AAPL', { someOption: true, etc });
+
+// sendFile will go here
+// app.get('/js', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'static/index.html'));
+// });
+
+app.get('/api/:stock', async (req, res) => {
+    try {
+        const query = req.params.stock;
+        const queryOptions = { period1: '2021-02-01' };
+        const result = await yahooFinance.historical(query, queryOptions);
+        res.send(result)
+    }
+    catch (err) {
+        console.log(err);
+        res.send(err);
+    }
 })
 
 // sendFile will go here
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'static/index.html'));
-  });
+});
 
 
 
